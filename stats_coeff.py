2d985fa092a36c9571f856a2a2912ace8dcb1e2f
MxPerrot <maxime.perrot@etudiant.univ-rennes1.fr>
2024-12-18 00:51:38 +0100

    docs: add comments to the secondary programs

--- stats_coeff.py
+++ stats_coeff.py
@@ -1,4 +1,22 @@
-def get_stats_coeff(pok1: list, pok2: list) -> float:
+# coding: utf-8 
+
+"""
+This script defines the functions to get the stats coefficient of two pokemons
+
+Author: Damien Goupil
+Date: 2024-12-19
+"""
+
+def get_stats_coeff(pok1: list, pok2: list, speed_modifier: float = 0.1) -> float:
+    """
+    Get the stats coefficient of the first pokemon against the second pokemon
+
+    pok1: list - the first pokemon
+    pok2: list - the second pokemon
+
+    return: float - the stats coefficient of the first pokemon against the second pokemon
+    """
+    
     coeffAtk     =   pok1['Attack']  / pok2['Defense']  
     coeffAtkSpe  =   pok1['Sp. Atk'] / pok2['Sp. Def']  
     coeffDef     =   pok1['Defense'] / pok2['Attack']  
--- type_effectiveness_coeff.py
+++ type_effectiveness_coeff.py
@@ -1,12 +1,28 @@
-import pandas as pd
-import numpy as np
-import math
+# coding: utf-8 
+
+"""
+This script defines the functions to get type effectiveness coefficient of the attack types against the defense types of two pokemons
+
+Author: Ewan Lansonneur
+Date: 2024-12-19
+"""
+
 import json
 
 with open('type_chart.json') as f:
     type_effectiveness = json.load(f)
 
 def get_type_effectiveness_coeff(typeAtk1: str, typeAtk2: str, typeDef1: str, typeDef2: str, show=False) -> float: 
+    """
+    Get the type effectiveness coefficient of the attack types against the defense types
+    
+    typeAtk1: str - the first attack type
+    typeAtk2: str - the second attack type
+    typeDef1: str - the first defense type
+    typeDef2: str - the second defense type
+    
+    return: float - the type effectiveness coefficient of the attack types against the defense types
+    """
 
     coeff = type_effectiveness[typeAtk1][typeDef1] 
 
